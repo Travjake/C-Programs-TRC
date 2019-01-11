2ef73ed4f746bd83b90b3e1fd53ef46913b416c9
Travis Chapple <000029486@student.thomroth.ac.uk>
2019-01-11 10:05:17 +0000

    2012 Question Complete

---
+++ 2012 Test Question/2012 Test Question/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2012_Test_Question
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ans = 0;
+            int col = 8;
+            while (col >= 1)
+            {
+                Console.WriteLine("Enter Bit Value (From left): ");
+                int bv = Convert.ToInt16(Console.ReadLine());
+                ans = ans + (col * bv);
+                col = col / 2;
+            }
+            Console.WriteLine("The decimal value is: " + ans);
+            Console.ReadLine();
+        }
+    }
+}
