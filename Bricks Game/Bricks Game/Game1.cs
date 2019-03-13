ffa05206e798e1d67da7065d0be75d38ebe142e1
Travis Chapple <000029486@student.thomroth.ac.uk>
2019-03-13 09:02:23 +0000

    Added 2019 Res

---
+++ Bricks Game/Bricks Game/Game1.cs
@@ -0,0 +1,270 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace Bricks_Game
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        public Texture2D imgBrick;
+        public Texture2D imgPaddle;
+        public Texture2D imgBall;
+        public Texture2D imgPixel;
+        public SoundEffect startSound;
+        public SoundEffect brickSound;
+        public SoundEffect paddleBounceSound;
+        public SoundEffect wallBounceSound;
+        public SoundEffect missSound;
+        public SpriteFont youwin;
+        SpriteFont Font;
+
+        public Vector2 paddlePosition;
+        public Vector2 ballMovement;
+        public Vector2 ballPosition;
+        public bool ballInMotion = false;
+        public int speed = 8;
+
+        Rectangle paddleRectangle;
+        Rectangle ballRectangle;
+
+        const int rows = 5;
+        const int cols = 8;
+        bool[,] brickMap = new bool[rows, cols];
+        byte check = 0;
+        int BricksKo = 0;
+
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferHeight = 600;
+            graphics.PreferredBackBufferWidth = 400;
+            graphics.ApplyChanges();
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            Rectangle paddleRectangle;
+            Rectangle ballRectangle;
+
+            // TODO: use this.Content to load your game content here
+            //load images
+            imgBall = Content.Load<Texture2D>("Ball");
+            imgPixel = Content.Load<Texture2D>("Pixel");
+            imgPaddle = Content.Load<Texture2D>("Paddle");
+            imgBrick = Content.Load<Texture2D>("Brick");
+
+            //load sounds
+            startSound = Content.Load<SoundEffect>("StartSound");
+            brickSound = Content.Load<SoundEffect>("BrickSound");
+            paddleBounceSound = Content.Load<SoundEffect>("PaddleBounceSound");
+            wallBounceSound = Content.Load<SoundEffect>("WallBounceSound");
+            missSound = Content.Load<SoundEffect>("MissSound");
+
+            //load text
+            youwin = Content.Load<SpriteFont>("Bold");
+
+            //load font
+            Font = Content.Load<SpriteFont>("Regular");
+
+
+            paddlePosition = new Vector2((graphics.PreferredBackBufferWidth / 2) - imgPaddle.Width, 550);
+
+           
+
+            //// Stay at end 
+            SetBricks();
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            paddleRectangle = new Rectangle((int)paddlePosition.X, (int)paddlePosition.Y, imgPaddle.Width, imgPaddle.Height);
+            ballRectangle = new Rectangle((int)ballPosition.X, (int)ballPosition.Y, imgBall.Width, imgBall.Height);
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Z))
+            {
+                if (paddlePosition.X < 0)
+                    paddlePosition.X = 0;
+                else
+                    paddlePosition.X -= 2;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.X))
+            {
+                if (paddlePosition.X > (graphics.PreferredBackBufferWidth - imgPaddle.Width))
+                    paddlePosition.X = graphics.PreferredBackBufferWidth - imgPaddle.Width;
+                else
+                    paddlePosition.X += 2;
+            }
+
+            if (!ballInMotion)
+                ballPosition = new Vector2(paddlePosition.X + (imgPaddle.Width / 2) - (imgBall.Width / 2),
+                    paddlePosition.Y - imgBall.Height);
+            else
+                ballPosition += ballMovement;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                ballInMotion = true;
+                ballMovement = new Vector2(-2, -(speed));
+                check = 1;
+            }
+
+            if (paddleRectangle.Intersects(ballRectangle))
+            {
+                ballPosition.Y = paddlePosition.Y - imgBall.Height;
+                ballMovement = Bounce(ballMovement, true, paddleBounceSound);
+            }
+
+            //check for bricks
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    if (brickMap[r, c] && ballRectangle.Intersects(new Rectangle(c * imgBrick.Width, r * imgBrick.Height, imgBrick.Width, imgBrick.Height)))
+                    {
+                        brickMap[r, c] = false;
+                        ballMovement = Bounce(ballMovement, true, brickSound);
+                        BricksKo++;
+                    }
+                }
+            }
+            if(ballPosition.X <= 0 || ballPosition.X >= graphics.PreferredBackBufferWidth - imgBall.Width)
+            {
+                ballMovement = Bounce(ballMovement, false, brickSound);
+            }
+
+            if (ballPosition.Y <= 0)
+            {
+                ballMovement = Bounce(ballMovement, true, brickSound);
+            }
+
+            if (ballPosition.Y >= graphics.PreferredBackBufferHeight + imgBall.Height)
+            {
+                ballPosition.X = paddlePosition.X + imgPaddle.Width / 2;
+                ballPosition.Y = paddlePosition.Y - imgBall.Height;
+                ballMovement = new Vector2(0,0);
+                ballInMotion = false;
+            }
+
+            if (BricksKo == 39)
+            {
+                spriteBatch.Begin();
+                spriteBatch.DrawString(Font, "You Win!!!!", new Vector2(0, graphics.PreferredBackBufferHeight), Color.White);
+                spriteBatch.End();
+            }
+
+            
+
+        }
+
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            spriteBatch.Begin();
+            spriteBatch.DrawString(Font, "Bricks Hit: " + BricksKo, new Vector2(0, graphics.PreferredBackBufferHeight - 100), Color.White);
+            spriteBatch.End();
+
+            GraphicsDevice.Clear(Color.Black);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+
+            spriteBatch.Begin();
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    if (brickMap[r, c])
+                        spriteBatch.Draw(imgBrick, new Rectangle(c * imgBrick.Width, r * imgBrick.Height, imgBrick.Width, imgBrick.Height), Color.Blue);
+                }
+            }
+            spriteBatch.Draw(imgPaddle, paddlePosition);
+            spriteBatch.Draw(imgBall, ballPosition);
+            spriteBatch.End();
+
+
+        }
+
+        /////////For Bouncy Ball
+
+        public Vector2 Bounce(Vector2 current, bool Vertical, SoundEffect sound)
+        {
+            if (Vertical)
+                current = new Vector2(current.X, -(current.Y));
+            else
+                current = new Vector2(-(current.X), current.Y);
+
+            sound.Play();
+
+            return current;
+        }
+
+        //////// Bricks 
+        public void SetBricks()
+        {
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    brickMap[r, c] = true;
+                }
+            }
+        }
+
+    }
+}
---
+++ My 2019 AS Res/My 2019 AS Res/Game1.cs
@@ -0,0 +1,208 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace My_2019_AS_Res
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        SpriteFont Label;
+        //Textures
+        public Texture2D BlackSquare;
+        public Texture2D TopBorder;
+        public Texture2D BottomBorder;
+        public Texture2D LeftBorder;
+        public Texture2D RightBorder;
+        Texture2D WH, WL, BH, BL;
+        Texture2D pixel;
+        //Vectors
+
+        //Form the Grid
+        const int rows = 8;
+        const int cols = 8;
+        bool[,] background = new bool[rows, cols];
+
+        Board Grid = new Board();
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferHeight = 900;
+            graphics.PreferredBackBufferWidth = 900;
+            graphics.ApplyChanges();
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+           
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+          
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            
+            BlackSquare = Content.Load<Texture2D>("Black Square");
+            WH = Content.Load<Texture2D>("White Higher");
+            WL = Content.Load<Texture2D>("White Lower");
+            BH = Content.Load<Texture2D>("Black Higher");
+            BL = Content.Load<Texture2D>("Black Lower");
+
+            SetBackground();
+
+            Label = Content.Load<SpriteFont>("Regular");
+
+            pixel = new Texture2D(GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
+            pixel.SetData(new[] { Color.Black }); // so that we can draw whatever color we want on top of it 
+
+            Grid.Init();
+            SetCounters(Grid);
+            // TODO: use this.Content to load your game content here
+        }
+
+        public void SetCounters(Board B)
+        {
+            // foreach(Square S in B.grid)
+            //{
+            // if(S.SquareColour == Color.White && S.Y < 200)
+            //  {
+            //     S.counter = WL;
+            // }
+            //  if (S.SquareColour == Color.White && S.Y >= 600)
+            // {
+            //     S.counter = BL;
+            // }
+            // }
+
+            for (int ycor = 0; ycor < 8; ycor++)
+            {
+                for (int xcor = 0; xcor < 8; xcor++)
+                {
+                    if(Grid.grid[xcor,ycor].SquareColour == Color.White && Grid.grid[xcor, ycor].Y * 100 < 200)
+                    {
+                        Grid.grid[xcor, ycor].counter = WL;
+                        Console.WriteLine("Counter Placed");
+                    }
+                    if (Grid.grid[xcor, ycor].SquareColour == Color.White && Grid.grid[xcor, ycor].Y * 100 >= 600)
+                    {
+                        Grid.grid[xcor, ycor].counter = BL;
+                    }
+                    Console.WriteLine((Grid.grid[xcor, ycor].counter != null) + " Counter Created" + xcor + " " + ycor);
+                    Console.WriteLine((Grid.grid[xcor, ycor].SquareColour) + " Counter Created" + xcor + " " + ycor);
+                }
+            }
+        }
+       
+        
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+           
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.White);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    if (background[r, c])
+                        spriteBatch.Draw(BlackSquare, new Rectangle(c * BlackSquare.Width, r * BlackSquare.Height, BlackSquare.Width, BlackSquare.Height), Color.Blue);
+                }
+            }
+            spriteBatch.End();
+
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(pixel, new Rectangle(0, 0, 800, 2), Color.Black);//Top
+            spriteBatch.Draw(pixel, new Rectangle(0, 800, 802, 2), Color.Black);//Bottom
+            spriteBatch.Draw(pixel, new Rectangle(0, 0, 2, 800), Color.Black);//Left
+            spriteBatch.Draw(pixel, new Rectangle(800, 0, 2, 800), Color.Black);//Right
+           
+
+            Vector2 start = new Vector2(820, 40);
+            Vector2 increm = new Vector2(0, 100);
+            for (int i = 0; i < 8; i++)
+            {
+                spriteBatch.DrawString(Label, Convert.ToString(i+1), start, Color.Black);
+                start += increm;
+            }
+
+            start = new Vector2(47, 820);
+            increm = new Vector2(100, 0);
+            for (int i = 0; i < 8; i++)
+            {
+                spriteBatch.DrawString(Label, Convert.ToString(i + 1), start, Color.Black);
+                start += increm;
+            }
+
+            Grid.Draw(spriteBatch);
+
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+
+        public void SetBackground()
+        {
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    if((r + c) % 2 == 1)
+                        background[r, c] = true;   
+                }
+                //else background[r, c] = false;
+            }
+        }
+    }
+}
